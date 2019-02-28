a6524f5be4ff21e71725c024fbcad1f5ae92f55d
Kane Koo <kanekoo@gmail.com>
2019-02-28 03:21:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -41,7 +41,7 @@ def hit? (card_total)
   user_input = get_user_input
   if (user_input == "h") 
     card_total = card_total + deal_card
-  elsif (user_input != "h" )
+  elsif (user_input != "s")
   # code hit? here
 end
 
